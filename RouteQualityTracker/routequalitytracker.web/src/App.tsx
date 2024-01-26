968c10af0ea77c6e9f1cd24bf4c52a2a6b69f384
Filip Bieleszuk <filip.bieleszuk@unit4.com>
2024-01-26 15:58:11 +0100

    render Gpx from user file

--- RouteQualityTracker/routequalitytracker.web/src/App.tsx
+++ RouteQualityTracker/routequalitytracker.web/src/App.tsx
@@ -1,25 +1,52 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 import { MapContainer, Polyline, TileLayer } from "react-leaflet";
-import GpxFileContent from "./assets/track2.gpx?raw";
 import { GpxFile, parseGpx } from "./GpxFileParser";
 
 function App() {
   const [gpxFile, setGpxFile] = useState<GpxFile>();
+  const [selectedFile, setSelectedFile] = useState<File>();
+  const [gpxFileContent, setGpxFileContent] = useState<string>();
 
   useEffect(() => {
+    if (selectedFile === undefined) {
+      setGpxFileContent(undefined);
+      setGpxFile(undefined);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setGpxFileContent(reader.result as string);
+    };
+    reader.readAsText(selectedFile);
+  }, [selectedFile]);
+
+  useEffect(() => {
+    if (gpxFileContent === undefined) return;
+
     const gpxData = new DOMParser().parseFromString(
-      GpxFileContent,
+      gpxFileContent,
       "application/xml"
     );
 
     setGpxFile(parseGpx(gpxData));
-  }, []);
+  }, [gpxFileContent]);
+
+  function changeGpxFile(event: ChangeEvent<HTMLInputElement>): void {
+    const selectedFile = event.target.files?.[0];
+    console.log(`selected file: ${selectedFile?.name}`);
+    setSelectedFile(selectedFile);
+  }
 
   return (
     <>
       <h1>Route Quality</h1>
-      {gpxFile ? (
+      <div>
+        <p>Select file to render:</p>
+        <input type="file" onChange={changeGpxFile} />
+      </div>
+      {gpxFile && (
         <>
           <h2>{gpxFile.name}</h2>
           <MapContainer
@@ -46,8 +73,6 @@ function App() {
             })}
           </MapContainer>
         </>
-      ) : (
-        <>Loading...</>
       )}
     </>
   );
